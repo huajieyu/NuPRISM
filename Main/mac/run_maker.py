510bca359b97549507e7a3b04835c20696be0a9c
huajieyu <libojiang8584@gmail.com>
2019-09-17 10:16:39 -0500

    renamed the output root file

--- Main/mac/run_maker.py
+++ Main/mac/run_maker.py
@@ -8,16 +8,16 @@ from ROOT import Main
 maker = Main.Maker()
 
 
-calc_syst_list = ["PerfectReconstruction_neutronsDetected"]
+#calc_syst_list = ["PerfectReconstruction_neutronsDetected"]
 #calc_syst_list = ["PerfectReconstruction_neutronsUndetected"]
 #calc_syst_list = ["OurSetup_neutronsDetected"]
 #calc_syst_list = ["OurSetup_neutronsUndetected"]
-#calc_syst_list = ["DUNE_FastMC"]
+calc_syst_list = ["DUNE_FastMC"]
 
 for systname in calc_syst_list:
 	maker.SetInputFile("/pnfs/genie/persistent/users/jiangl/GENIE_sample/gntp.1000.gst.286.root");
 
-	maker.SetOutputFile("/genie/app/users/jiangl/NuPRISM_output/output_test"+systname+".root");
+	maker.SetOutputFile("/genie/app/users/jiangl/NuPRISM_output/output_"+systname+".root");
 
 	maker.SetNeutrinoFlavor(0);  # the options are 0(numu), 1(anumu), 2(nue, 3(anue)
 
