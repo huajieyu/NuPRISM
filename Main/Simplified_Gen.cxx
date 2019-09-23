141249cea2fe84a604b43fdf16e4a7bb7740e2ec
huajieyu <libojiang8584@gmail.com>
2019-09-22 22:45:10 -0500

    added Simplified_Gen  for the simplified calculation of Enu

---
+++ Main/Simplified_Gen.cxx
@@ -0,0 +1,274 @@
+#ifndef __MAIN_SIMPLIFIED_GEN_CXX__
+#define __MAIN_SIMPLIFIED_GEN_CXX__
+
+#include "Simplified_Gen.h"
+
+#include <TArray2D.h>
+#include <TArray3D.h>
+using namespace std;
+using namespace Base;
+
+void Main::Simplified_Gen::SetInputFile(std::string in)
+{
+  filen = in;
+}
+void Main::Simplified_Gen::SetOutputFile(std::string in)
+{
+  fileoutn = in;
+}
+void Main::Simplified_Gen::PrintConfig()
+{
+  //LOG_INFO()<<"____Main::Simplified_Gen::PrintConfig"<<std::endl;
+
+}
+void Main::Simplified_Gen::DrawProgressBar(double progress, double barWidth){
+  std::cout<<"[";
+  int pos = barWidth * progress;
+  for (int i=0; i< barWidth; i++){
+     if(i<pos) std::cout<<"=";
+     else if(i == pos) std::cout << ">";
+     else std::cout<<" ";
+  }
+  std::cout << "] " << int(progress * 100.0) << " %\r";
+  std::cout.flush();
+}
+
+void Main::Simplified_Gen::MakeFile(){
+
+
+  TFile *file_out = new TFile(fileoutn.c_str(), "RECREATE");
+  if ( file_out -> IsOpen() ){
+    //LOG_NORMAL()>>" File Opened Successfully. "<<std::endl;
+    std::cout <<" File Opened Successfully. "<<std::endl;
+  } else {
+    //LOG_CRITICAL() <<"File not Opened (maybe not found. File:"<<fileoutn<<std::endl;
+    std::cout <<"File not Opened (maybe not found. File:"<<fileoutn<<std::endl;
+    exit(0);
+  }
+  std::string pattern = filen;
+  
+  TChain *chain_nuprism;
+  chain_nuprism = new TChain ("gst");
+  chain_nuprism->Add(pattern.c_str());
+  int evts = chain_nuprism -> GetEntries();
+  int barWidth = 70;
+  std::cout<<"[Simplified_Gen]<<<<<Number of events used is: "<<evts<<std::endl;
+
+  GENIEEventHisto *t=new GENIEEventHisto(chain_nuprism);
+
+  _event_histo_1d = new DUNENDEventHisto1D();
+  _event_histo_1d->InitializeBootstraps();
+
+    
+    int finalParticleNumber(0);
+    double trueNuEnergy(0.0), leptonEnergy(0.0);
+    double recNuEnergy_cal[calOptions];
+
+    int eventCnt_cal[calOptions];
+
+    
+
+
+    for ( int kCnt(0); kCnt < calOptions; ++kCnt )
+    {
+        eventCnt_cal[kCnt] = 0;
+
+        recNuEnergy_cal[kCnt] = 0.0;
+    }
+
+    double neutronEnergy(0.0);
+
+    int eventCnt(0);
+
+    ///loop over all entries in the GENIE trees
+    //for (int iCnt = 49992; iCnt < 49993; iCnt++)
+    for (int iCnt = 0; iCnt < chain_nuprism->GetEntries(); ++iCnt)
+    {
+        chain_nuprism->GetEntry(iCnt);
+        //TArray3D<double>& CalMatrix(0, 0, 0);
+        //if(iCnt != 49892) continue;
+        std::cout<<"[Simplified_Gen] event number is "<<iCnt<<std::endl;   
+        double nuEnergy = t->Ev;
+        const int nx = round( (nuEnergy - eMin)/eStep); 
+
+    
+        finalParticleNumber = t->nf;
+        trueNuEnergy  = t->Ev;
+        leptonEnergy = t->El;
+
+
+ 
+        if (finalParticleNumber < 0)
+            continue;
+
+        for (int jCnt(0); jCnt < calOptions; ++jCnt) {
+            recNuEnergy_cal[jCnt] = leptonEnergy;
+        }
+
+    	double calorimetricContribution[calOptions];
+
+    	for ( int kCnt(0); kCnt < calOptions; ++kCnt ) {
+        	calorimetricContribution[kCnt] = 0.0;
+        }
+        ///till the whole event is read, it is not clear if we are going to keep it,
+        ///so the calorimetric contribution cannot be immediately added to calculate the reconstructed energy
+
+        
+
+
+        int  heavyBaryon_app = 0; 
+
+
+        for (int finalPartCnt=0; finalPartCnt < finalParticleNumber; ++finalPartCnt)
+        {  std::cout<<"<<<<<<<<<<<<<<<<<PDG code is "<<t->pdgf[finalPartCnt] <<std::endl;
+
+           ///calculate invariant mass here
+	    double invariantMass = sqrt( t->Ef[finalPartCnt]*t->Ef[finalPartCnt] - 
+					t->pxf[finalPartCnt]*t->pxf[finalPartCnt] - 
+					t->pyf[finalPartCnt]*t->pyf[finalPartCnt] - 
+					t->pzf[finalPartCnt]*t->pzf[finalPartCnt] );
+        
+            ///make sure that there are no nuclear remnants here!!!
+            
+            ///protons
+            if ( t->pdgf[finalPartCnt] == 2212 )
+            {   //std::cout<<"[Simplified_Gen] << Oooooooooops find a proton ! "<<"Energy is "<<t->Ef[finalPartCnt]<<"  pMass= "<<pMass<<std::endl;
+                for ( int kCnt(0); kCnt < calOptions; ++kCnt ) {
+        		//calorimetricContribution[kCnt] += t->Ef[finalPartCnt] - pMass; 
+        		calorimetricContribution[kCnt] += t->Ef[finalPartCnt] - invariantMass; 
+                }
+
+            }
+            //std::cout<<"test::::calorimetricContribution is "<<calorimetricContribution[0]<<std::endl;
+            ///antiprotons
+            ///if you have an antiproton, one of the protons was produced and not knocked out
+            ///so it should be treated differently above
+            ///here we correct its treatment adding its mass here
+            ///a produced pair contributes 2 total energies, for the proton we already added the kinetic energy above,
+            ///so for antiproton we add the total energy, plus the proton mass
+            else if ( t->pdgf[finalPartCnt] == -2212 )
+            {   //std::cout<<"[Simplified_Gen] << Oooooooooops find an anti-proton ! "<<std::endl; 
+                for ( int kCnt(0); kCnt < calOptions; ++kCnt ) {
+                        //calorimetricContribution[kCnt] += t->Ef[finalPartCnt] + pMass;
+        		calorimetricContribution[kCnt] += t->Ef[finalPartCnt] + invariantMass; 
+		}
+            }
+
+            else if ( t->pdgf[finalPartCnt] == 2112 )
+            {    std::cout<<"[Simplified_Gen] << Oooooooooops find a neutron ! "<<std::endl; 
+                for ( int kCnt(0); kCnt < calOptions; ++kCnt ) {
+                    //calorimetricContribution[kCnt] += (minNeutronEfficiency + kCnt*stepNeutronEfficiency)*(t->Ef[finalPartCnt] - nMass);
+                    calorimetricContribution[kCnt] += (minNeutronEfficiency + kCnt*stepNeutronEfficiency)*(t->Ef[finalPartCnt] - invariantMass);
+                }
+                neutronEnergy += t->Ef[finalPartCnt] - nMass;
+                
+            }
+
+            ///antineutrons, same story as for protons
+            else if ( t->pdgf[finalPartCnt] == -2112 )
+            {    std::cout<<"[Simplified_Gen] << Oooooooooops find an anti-neutron ! "<<std::endl;  
+                for ( int kCnt(0); kCnt < calOptions; ++kCnt ){
+                    //calorimetricContribution[kCnt] += (minNeutronEfficiency + kCnt*stepNeutronEfficiency)*(t->Ef[finalPartCnt] + nMass);
+                    calorimetricContribution[kCnt] += (minNeutronEfficiency + kCnt*stepNeutronEfficiency)*(t->Ef[finalPartCnt] + invariantMass);
+                }
+                neutronEnergy += t->Ef[finalPartCnt] + nMass;
+            }
+
+
+            ///heavy baryon
+            else if ( abs(t->pdgf[finalPartCnt]) == 3122 || abs(t->pdgf[finalPartCnt]) == 3222 
+                   || abs(t->pdgf[finalPartCnt]) == 3212 || abs(t->pdgf[finalPartCnt]) == 3112 
+	           || abs(t->pdgf[finalPartCnt]) == 3224 || abs(t->pdgf[finalPartCnt]) == 3214 
+                   || abs(t->pdgf[finalPartCnt]) == 3114 || abs(t->pdgf[finalPartCnt]) == 3322 
+                   || abs(t->pdgf[finalPartCnt]) == 3312 || abs(t->pdgf[finalPartCnt]) == 3324 
+                   || abs(t->pdgf[finalPartCnt]) == 3314 || abs(t->pdgf[finalPartCnt]) == 3334 
+                   || abs(t->pdgf[finalPartCnt]) == 4122 || abs(t->pdgf[finalPartCnt]) == 4222
+                   || abs(t->pdgf[finalPartCnt]) == 4212 || abs(t->pdgf[finalPartCnt]) == 4112
+                   || abs(t->pdgf[finalPartCnt]) > 10e5
+                   )
+            {
+                std::cout<<"Heavy baryon found. Should not be here!"<<std::endl;
+                heavyBaryon_app ++;
+                continue;
+            }
+
+            ///for pions, photons etc. add the total energy
+            else {
+                for ( int kCnt(0); kCnt < calOptions; ++kCnt ){
+                    calorimetricContribution[kCnt] += t->Ef[finalPartCnt];
+                }
+            } 
+
+        }
+        if(heavyBaryon_app > 0) {continue;}
+
+                for (int mCnt(0); mCnt < calOptions; ++mCnt) {
+                    recNuEnergy_cal[mCnt] += calorimetricContribution[mCnt];
+                    
+
+                    if(mCnt== calOptions -1 && -(recNuEnergy_cal[mCnt] - trueNuEnergy) > 1) {std::cout<<"test events appears "<<iCnt <<"true Eu "<< trueNuEnergy<< " reco Enu "<<recNuEnergy_cal[mCnt]<<std::endl;}
+                     _event_histo_1d->h_true_calc_Enu[mCnt]->Fill(trueNuEnergy, recNuEnergy_cal[mCnt]);
+
+                }
+
+
+
+                ++eventCnt;
+
+
+
+                //for ( int kCnt(0); kCnt < calOptions; ++kCnt )
+                //        if (   ( recNuEnergy_cal[kCnt] >= eMin ) and ( recNuEnergy_cal[kCnt] < eMax )   )
+                //    {
+                //        ++eventCnt_cal[kCnt];
+
+                //        const int ny(   static_cast<int>( (recNuEnergy_cal[kCnt] - eMin)/eStep )   );
+                //        CalMatrix(kCnt, nx, ny) += 1;
+                //    }
+
+
+
+
+
+    } //end of loop over all the entris
+
+
+
+
+
+  file_out->cd();
+  file_out->WriteObject(_event_histo_1d, "DUNENDEventHisto1D");
+
+
+  file_out->Write();
+  file_out->Close();
+
+
+
+
+
+
+
+
+
+
+
+
+
+}  //end of member function MakeFile
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
---
+++ Main/Simplified_Gen.h
@@ -0,0 +1,204 @@
+/**
+ * \file Simplified_Gen.h
+ *
+ * \ingroup Main
+ * 
+ * \brief Class def header for a class Simplified_Gen
+ *
+ * @author jiangl
+ */
+
+/** \addtogroup Main
+
+    @{*/
+#ifndef __MAIN_SIMPLIFIED_GEN_H__
+#define __MAIN_SIMPLIFIED_GEN_H__
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <ctime>
+#include <vector>
+#include <map>
+#include <stdlib.h>
+#include <stdio.h>
+#include <getopt.h>
+#include <fstream>
+#include <math.h>
+#include <algorithm>
+
+#include <TRandom1.h>
+#include <TSystem.h>
+#include <TBrowser.h>
+#include <TApplication.h>
+#include <TChain.h>
+#include "TThread.h"
+#include "THStack.h"
+#include "TLegend.h"
+#include "TEfficiency.h"
+#include "TGraphAsymmErrors.h"
+#include "TGraph.h"
+#include <TString.h>
+#include <TROOT.h>
+#include <TStyle.h>
+#include <TH1D.h>
+#include <TH2D.h>
+#include <TLatex.h>
+#include <TCanvas.h>
+#include "TMath.h"
+#include "TH2Poly.h"
+
+//#include "include/"
+//#include "include/"
+//#include "include/"
+
+#include "GENIEEventHisto.h"
+#include "../Base/DUNENDEventHisto1D.h"
+#include "../Base/CDetectorEffects.h"
+
+
+using namespace Base;
+
+namespace Main {
+
+  /**
+     \class Simplified_Gen
+     User defined class Simplified_Gen ... these comments are used to generate
+     doxygen documentation!
+  */
+  class Simplified_Gen{
+    
+  public:
+    
+    /// Default constructor
+    Simplified_Gen(){}
+    
+    /// Default destructor
+    ~Simplified_Gen(){}
+    void MakeFile();
+    void SetInputFile(std::string);
+    void SetOutputFile(std::string);
+
+    void SetNeutrinoFlavor(int flavorid){
+        neutrinoFlavor = flavorid;
+    };
+    void SetInteractionType(int interid){
+        interactionType = interid;
+    }
+    void SetExperimentName (std::string expname){
+        i_experiment = expname;    
+    }
+
+    void SetKinematicInfoFileName(std::string kinematicinfo_filename){
+        kinematicInfoName_string =  kinematicinfo_filename;    
+    }
+
+
+    void SetFSIOn(bool v){
+          switchon_FSI = v;
+
+    }
+
+    void PrintConfig();
+    void DrawProgressBar(double progress, double barWidth);
+
+  private:
+
+    int calOptions = 11;
+    double minNeutronEfficiency = 0.0;
+    double stepNeutronEfficiency = 0.1;
+    DUNENDEventHisto1D * _event_histo_1d;
+
+    double eMin = 0.2;
+    double eMax = 8.0;
+    double eStep = 0.1;
+
+    std::string filen = "";
+    std::string fileoutn = "output_maker.root";
+    int _initial_entry = 0; //Entry in tree to begin with    
+
+    int neutrinoFlavor = -1;
+    int interactionType = -1;
+    std::string i_experiment = "";
+   
+    std::string kinematicInfoName_string= "kinematicInfo.txt";
+    std::ofstream kinematicInfoFile;
+
+    bool switchon_FSI = true;
+
+    const double GeV = 1.0;
+    const double MeV = 0.001*GeV;
+
+
+    const double eMass =      0.510998928*MeV ;///electron mass
+    const double muMass =   105.6583715*MeV ;///muon mass
+    const double pMass =  938.272046*MeV ;///proton mass
+    const double nMass =  939.565379*MeV ;///neutron mass
+    const double piCMass =  139.57018*MeV ;///charged pion mass
+    const double pi0Mass =  134.9766*MeV ;///neutral pion mass
+    const double kaonCMass =   493.677*MeV ;///charged kaon mass
+    const double kaon0Mass =   497.614*MeV ;///neutral kaon mass
+    const double deltaMass =   1232*MeV ;///delta mass
+
+
+    const double eMassSq =  eMass*eMass ;///electron mass squared
+    const double muMassSq =  muMass*muMass ;///muon mass squared
+    const double pMassSq =  pMass*pMass ;///proton mass squared
+    const double nMassSq =  nMass*nMass ;///neutron mass squared
+    const double piCMassSq =  piCMass*piCMass ;///charged pion mass squared
+    const double pi0MassSq =  pi0Mass*pi0Mass ;///neutral pion mass squared
+    const double kaonCMassSq =  kaonCMass*kaonCMass ;///charged kaon mass squared
+    const double kaon0MassSq =  kaon0Mass*kaon0Mass ;///neutral kaon mass squared
+    const double deltaMassSq =  deltaMass*deltaMass ;///delta mass squared
+
+
+
+
+    const double accuracy =  0.001*MeV;
+
+    const double averagePT = 0*MeV;///maximal transverse momentum allowed to be interpreted as coming from the Fermi motion
+    const double maxInitMom = 265*MeV ;///cut of the high-momentum componet of the spectral function
+
+    const double ProtonBinding = 1.0*MeV; // binding energy
+    const double NeutronBinding = 13.0*MeV;// binding energy
+
+
+    //const double ProtonBinding = 3.0*MeV; // binding energy
+    //const double NeutronBinding = 14.0*MeV;// binding energy
+    //const double ProtonBinding = 2.6*MeV; // binding energy
+    //const double NeutronBinding = 15.3*MeV;// binding energy
+
+    enum ExperimentName
+    {
+        PerfectReconstruction_neutronsDetected,
+        PerfectReconstruction_neutronsUndetected,
+        OurSetup_neutronsDetected,
+        OurSetup_neutronsUndetected,
+        DUNE_FastMC
+    };
+
+    int get_leptonPDG(int code){
+         int leptonPDG= 0.0;
+         if(code == 14)  {leptonPDG = 13;}
+         if(code == -14)  {leptonPDG = -13;}
+         if(code == 12)  {leptonPDG = 11;}
+         if(code == -12)  {leptonPDG = -11;}
+         return leptonPDG;
+
+    }
+
+    enum EventType
+    {
+         oneTrack,
+         multiTrack,
+         allEvents
+    };
+
+
+    
+  };
+}
+
+#endif
+/** @} */ // end of doxygen group 
+
